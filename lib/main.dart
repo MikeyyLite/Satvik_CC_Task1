4f11ef2df1cf1009c21b15aa5456e7c6b220879e
MikeyyLite <119350994+MikeyyLite@users.noreply.github.com>
2022-12-25 08:08:08 +0530

    initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,32 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'second_page.dart';
+import 'database.dart';
+import 'dart:math';
 
-void main() {
-  runApp(const MyApp());
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+
+  runApp(Home());
+}
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MyApp(),
+    );
+  }
 }
 
 class MyApp extends StatefulWidget {
@@ -13,16 +37,84 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   int chosenIndex = 0;
-  void _onItemTapped(int index) {
+  int i = 0;
+  Future _onItemTapped(int index) async {
+    docIDs = [];
     setState(() {
       chosenIndex = index;
     });
+
+    await getDocId();
+
+    if (index == 1) {
+      return practice();
+    }
+    if (index == 0) {
+      docIDs = [];
+    }
   }
 
+  void _setText(title, answer) {
+    String text = "";
+    String text2 = "";
+    docIDs = [];
+    setState(() {
+      text = title;
+      text2 = answer;
+    });
+  }
+
+  Future updateData(String question, String answer) async {
+    final docUser = FirebaseFirestore.instance
+        .collection('flashcards')
+        .doc()
+        .set({'question': question, 'answer': answer});
+    debugPrint('----------------------------updated---------------------');
+  }
+
+  List<String> docIDs = [];
+
+  Future getDocId() async {
+    await FirebaseFirestore.instance.collection('flashcards').get().then(
+          (snapshot) => snapshot.docs.forEach(
+            (document) {
+              docIDs.add(document.reference.id);
+            },
+          ),
+        );
+  }
+
+  Future deleteButtonPressed(index) async {
+    await FirebaseFirestore.instance
+        .collection('flashcards')
+        .doc(docIDs[index])
+        .delete();
+    docIDs = [];
+    setState(() {});
+  }
+
+  practice() {
+    var randomItem = (docIDs.toList()..shuffle()).first;
+
+    print('');
+    print('');
+    print(docIDs);
+    docIDs = [];
+
+    return Navigator.push(context, MaterialPageRoute(builder: (context) {
+      return SecondPage(
+        title: 'SecondPage',
+        documenntid: randomItem,
+      );
+    }));
+  }
+
+  String answer = "";
+  String title = "";
+  String text = "";
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        home: Scaffold(
+    return Scaffold(
       backgroundColor: Colors.white30,
       appBar: AppBar(
         title: const Text("FLASHCARDS",
@@ -39,80 +131,136 @@ class _MyAppState extends State<MyApp> {
         elevation: 0.00,
         backgroundColor: Colors.black,
       ),
-      body: GridView.builder(
-        itemCount: 5,
-        gridDelegate:
-            const SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
-        itemBuilder: (context, index) {
-          return Card(
-            color: Colors.black,
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(30.0),
-            ),
-            child: Column(
-              children: [
-                TextButton(
-                  onPressed: () {
-                    Navigator.push(context,
-                        MaterialPageRoute(builder: (context) {
-                      return const SecondPage(title: 'SecondPage');
-                    }));
-                  },
-                  child: Container(
-                      padding: const EdgeInsets.fromLTRB(30.0, 20.0, 30.0, 0.0),
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          const Padding(
-                            padding: EdgeInsets.fromLTRB(0.0, 8.0, 8.0, 8.0),
-                            child: Text(
-                              'Text',
-                              textAlign: TextAlign.left,
-                              style:
-                                  TextStyle(color: Colors.white, fontSize: 20),
-                            ),
-                          ),
-                          const Padding(
-                            padding: EdgeInsets.fromLTRB(0.0, 5.0, 8.0, 8.0),
-                            child: Text(
-                              'Answer',
-                              textAlign: TextAlign.left,
-                              style:
-                                  TextStyle(color: Colors.white, fontSize: 20),
-                            ),
-                          ),
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.end,
+      body: FutureBuilder(
+        future: getDocId(),
+        builder: (context, snapshot) {
+          return GridView.builder(
+            itemCount: docIDs.length,
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2),
+            itemBuilder: (context, index) {
+              return Card(
+                color: Colors.black,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(30.0),
+                ),
+                child: Column(
+                  children: [
+                    TextButton(
+                      onPressed: () {
+                        Navigator.push(context,
+                            MaterialPageRoute(builder: (context) {
+                          return SecondPage(
+                            title: 'SecondPage',
+                            documenntid: docIDs[index],
+                          );
+                        }));
+                      },
+                      child: Container(
+                          padding:
+                              const EdgeInsets.fromLTRB(30.0, 20.0, 30.0, 0.0),
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
                             children: [
                               Padding(
-                                padding: const EdgeInsets.fromLTRB(
-                                    0.0, 5.0, 8.0, 0.0),
-                                child: IconButton(
-                                  icon: const Icon(Icons.delete,
-                                      color: Colors.white, size: 40.0),
-                                  tooltip: 'Delete',
-                                  onPressed: () {},
+                                padding:
+                                    EdgeInsets.fromLTRB(0.0, 8.0, 8.0, 8.0),
+                                child: Container(
+                                  height: 80,
+                                  child: (Text(
+                                    'CARD $index',
+                                    textAlign: TextAlign.left,
+                                    style: TextStyle(
+                                        color: Colors.white, fontSize: 20),
+                                  )),
                                 ),
                               ),
+                              Row(
+                                // mainAxisAlignment: MainAxisAlignment.end,
+                                children: [
+                                  Container(
+                                    width: 62,
+                                  ),
+                                  IconButton(
+                                      icon: const Icon(Icons.delete,
+                                          color: Colors.white, size: 40.0),
+                                      tooltip: 'Delete',
+                                      onPressed: () =>
+                                          deleteButtonPressed(index)),
+                                ],
+                              ),
                             ],
-                          ),
-                        ],
-                      )),
+                          )),
+                    ),
+                  ],
                 ),
-              ],
-            ),
+              );
+            },
           );
         },
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          // Add your onPressed code here!
-        },
+        onPressed: () => showDialog(
+            context: context,
+            builder: (context) {
+              return Container(
+                child: SimpleDialog(
+                  elevation: 20,
+                  title: const Text('Enter New Card Info.'),
+                  children: <Widget>[
+                    Padding(
+                      padding: const EdgeInsets.all(15),
+                      child: TextField(
+                        decoration:
+                            const InputDecoration(labelText: 'Question'),
+                        onChanged: (value) => title = value,
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(15),
+                      child: TextField(
+                        decoration: const InputDecoration(labelText: 'Answer'),
+                        onChanged: (value) => answer = value,
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 8,
+                    ),
+
+                    ElevatedButton(
+                        onPressed: () {
+                          _setText(title, answer);
+                          updateData(title, answer);
+                          Navigator.pop(context);
+                          docIDs = [];
+                        },
+                        style: ButtonStyle(
+                            elevation: MaterialStateProperty.all(8),
+                            backgroundColor:
+                                MaterialStateProperty.all(Colors.green)),
+                        child: const Text('Submit')),
+
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    Text(text),
+
+                    // changes in text
+                    // are shown here
+                  ],
+                  //   SimpleDialogOption(
+                  //     onPressed: () {},
+                  //     child: const Text('Option 1'),
+                  //   ),
+                  //   SimpleDialogOption(
+                  //     onPressed: () {},
+                  //     child: const Text('Option 2'),
+                  //   ),
+                ),
+              );
+            }),
         backgroundColor: Colors.white,
-        child: const Icon(
-          Icons.add,
-          color: Colors.black
-        ),
+        child: const Icon(Icons.add, color: Colors.black),
       ),
       // ignore: unnecessary_new
       bottomNavigationBar: new Theme(
@@ -137,6 +285,73 @@ class _MyAppState extends State<MyApp> {
           onTap: _onItemTapped,
         ),
       ),
-    ));
+    );
+
+    // Widget buildTiles(question, answer) => GridView.builder(
+    //       itemCount: 5,
+    //       gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+    //           crossAxisCount: 2),
+    //       itemBuilder: (context, index) {
+    //         return Card(
+    //           color: Colors.black,
+    //           shape: RoundedRectangleBorder(
+    //             borderRadius: BorderRadius.circular(30.0),
+    //           ),
+    //           child: Column(
+    //             children: [
+    //               TextButton(
+    //                 onPressed: () {
+    //                   Navigator.push(context,
+    //                       MaterialPageRoute(builder: (context) {
+    //                     return const SecondPage(title: 'SecondPage');
+    //                   }));
+    //                 },
+    //                 child: Container(
+    //                     padding:
+    //                         const EdgeInsets.fromLTRB(30.0, 20.0, 30.0, 0.0),
+    //                     child: Column(
+    //                       mainAxisAlignment: MainAxisAlignment.spaceBetween,
+    //                       children: [
+    //                         const Padding(
+    //                           padding: EdgeInsets.fromLTRB(0.0, 8.0, 8.0, 8.0),
+    //                           child: Text(
+    //                             'Text',
+    //                             textAlign: TextAlign.left,
+    //                             style: TextStyle(
+    //                                 color: Colors.white, fontSize: 20),
+    //                           ),
+    //                         ),
+    //                         const Padding(
+    //                           padding: EdgeInsets.fromLTRB(0.0, 5.0, 8.0, 8.0),
+    //                           child: Text(
+    //                             'Answer',
+    //                             textAlign: TextAlign.left,
+    //                             style: TextStyle(
+    //                                 color: Colors.white, fontSize: 20),
+    //                           ),
+    //                         ),
+    //                         Row(
+    //                           mainAxisAlignment: MainAxisAlignment.end,
+    //                           children: [
+    //                             Padding(
+    //                               padding: const EdgeInsets.fromLTRB(
+    //                                   0.0, 5.0, 8.0, 0.0),
+    //                               child: IconButton(
+    //                                 icon: const Icon(Icons.delete,
+    //                                     color: Colors.white, size: 40.0),
+    //                                 tooltip: 'Delete',
+    //                                 onPressed: () {},
+    //                               ),
+    //                             ),
+    //                           ],
+    //                         ),
+    //                       ],
+    //                     )),
+    //               ),
+    //             ],
+    //           ),
+    //         );
+    //       },
+    //     );
   }
 }
--- lib/second_page.dart
+++ lib/second_page.dart
@@ -1,86 +1,119 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+import 'package:firebase_core/firebase_core.dart';
 
 class SecondPage extends StatelessWidget {
-  const SecondPage({Key? key, required this.title}) : super(key: key);
+  final String documenntid;
   final String title;
+  SecondPage({Key? key, required this.title, required this.documenntid})
+      : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        backgroundColor: Colors.black,
-        appBar: AppBar(
-          title: const Text("FLASHCARDS",
-              style: TextStyle(color: Colors.white, fontSize: 30)),
-          toolbarHeight: 70,
-          toolbarOpacity: 1,
-          titleSpacing: 0,
-          centerTitle: true,
-          shape: const RoundedRectangleBorder(
-            borderRadius: BorderRadius.only(
-                bottomRight: Radius.circular(20),
-                bottomLeft: Radius.circular(20)),
-          ),
-          elevation: 0,
-          backgroundColor: Colors.white12,
-        ),
-        body: Container(
-          child: Column(
-            children: [
-              Padding(
-                padding: const EdgeInsets.fromLTRB(8.0, 20.0, 8.0, 8.0),
-                child: SizedBox(
-                  height: 300,
-                  width: 800,
-                  child: Card(
-                      color: Colors.white12,
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(30.0),
+    CollectionReference flashcards =
+        FirebaseFirestore.instance.collection('flashcards');
+
+    return FutureBuilder<DocumentSnapshot>(
+        future: flashcards.doc(documenntid).get(),
+        builder: ((context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.done) {
+            Map<String, dynamic> data =
+                snapshot.data!.data() as Map<String, dynamic>;
+            print(data);
+
+            return Scaffold(
+                backgroundColor: Colors.black,
+                appBar: AppBar(
+                  title: const Text("FLASHCARDS",
+                      style: TextStyle(color: Colors.white, fontSize: 30)),
+                  toolbarHeight: 70,
+                  toolbarOpacity: 1,
+                  titleSpacing: 0,
+                  centerTitle: true,
+                  shape: const RoundedRectangleBorder(
+                    borderRadius: BorderRadius.only(
+                        bottomRight: Radius.circular(20),
+                        bottomLeft: Radius.circular(20)),
+                  ),
+                  elevation: 0,
+                  backgroundColor: Colors.white12,
+                ),
+                body: Container(
+                  child: Column(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.fromLTRB(8.0, 20.0, 8.0, 8.0),
+                        child: SizedBox(
+                          height: 300,
+                          width: 800,
+                          child: Card(
+                              color: Colors.white12,
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(30.0),
+                              ),
+                              child: Column(
+                                children: [
+                                  Padding(
+                                    padding: const EdgeInsets.fromLTRB(
+                                        0.0, 50.0, 0.0, 0.0),
+                                    child: Text(
+                                      'CARD $documenntid',
+                                      style: TextStyle(
+                                          color: Colors.white, fontSize: 20),
+                                    ),
+                                  ),
+                                  SizedBox(
+                                    height: 80,
+                                    width: 800,
+                                  ),
+                                  Text(
+                                    '${data['question']}',
+                                    style: TextStyle(
+                                        color: Colors.white, fontSize: 60),
+                                  ),
+                                ],
+                              )),
+                        ),
                       ),
-                      child: Column(
+                      SizedBox(
+                        height: 45,
+                        width: double.infinity,
+                      ),
+                      Row(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                         children: [
-                          Padding(
-                            padding:
-                                const EdgeInsets.fromLTRB(0.0, 50.0, 0.0, 0.0),
+                          TextButton(
+                            onPressed: () {},
                             child: Text(
-                              'Card #1',
+                              'Show Answer',
                               style:
-                                  TextStyle(color: Colors.white, fontSize: 20),
+                                  TextStyle(color: Colors.white, fontSize: 30),
                             ),
                           ),
-                          SizedBox(
-                            height: 80,
-                            width: 800,
-                          ),
-                          Text(
-                            'TITLE',
-                            style: TextStyle(color: Colors.white, fontSize: 60),
-                          ),
+                          IconButton(
+                              icon: const Icon(Icons.navigate_next,
+                                  color: Colors.white, size: 60.0),
+                              onPressed: () => showDialog(
+                                  context: context,
+                                  builder: (context) {
+                                    return Container(
+                                        child: AlertDialog(
+                                      elevation: 20,
+                                      title: Text(
+                                        ' Q| ${data['question']} \n \n A| ${data['answer']}',
+                                        style: TextStyle(fontSize: 20),
+                                      ),
+                                    ));
+                                  })),
                         ],
-                      )),
-                ),
-              ),
-              SizedBox(
-                height: 45,
-                width: double.infinity,
-              ),
-              Row(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  TextButton(
-                    onPressed: () {},
-                    child: Text(
-                      'Show Answer',
-                      style: TextStyle(color: Colors.white, fontSize: 30),
-                    ),
+                      ),
+                    ],
                   ),
-                  IconButton(
-                      icon: const Icon(Icons.navigate_next,
-                          color: Colors.white, size: 60.0),
-                      onPressed: () {}),
-                ],
-              ),
-            ],
-          ),
-        ));
+                ));
+          }
+          return Text('');
+        }));
   }
 }
